7c59440246cc722fcede9c6a72b5c79596cbea93
bmb tech <techibmb@gmail.com>
2026-01-07 15:41:20 +0300

    Update OWNER_NAME and BOT values with new names

--- settings.js
+++ settings.js
@@ -11,11 +11,11 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
 module.exports = {
     session: process.env.SESSION_ID || '',
     PREFIXE: process.env.PREFIX || "+",
-    OWNER_NAME: process.env.OWNER_NAME || "B.M.B-TECH",
+    OWNER_NAME: process.env.OWNER_NAME || "𝐛𝐦𝐛 𝐭𝐞𝐜𝐡",
     NUMERO_OWNER : process.env.NUMERO_OWNER || "25576786×××××",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
-    BOT : process.env.BOT_NAME || 'B.M.B-TECH',
+    BOT : process.env.BOT_NAME || '𝐛𝐦𝐛 𝐭𝐞𝐜𝐡',
     URL : process.env.BOT_MENU_LINKS || 'https://files.catbox.moe/8qq3l4.jpg',
     MODE: process.env.PUBLIC_MODE || "yes",
     PM_PERMIT: process.env.PM_PERMIT || 'yes',
