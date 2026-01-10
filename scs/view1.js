d22e836997dcdfeb5cc922b33b0c0026dcd6387c
bmb tech <techibmb@gmail.com>
2026-01-10 23:57:24 +0300

    Create view1.js

---
+++ scs/view1.js
@@ -0,0 +1,97 @@
+const { bmbtz } = require('../devbmb/bmbtz');
+const axios = require("axios")
+let { Sticker, createSticker, StickerTypes } = require('wa-sticker-formatter');
+const {isUserBanned , addUserToBanList , removeUserFromBanList} = require("../lib/banUser");
+const  {addGroupToBanList,isGroupBanned,removeGroupFromBanList} = require("../lib/banGroup");
+const {isGroupOnlyAdmin,addGroupToOnlyAdminList,removeGroupFromOnlyAdminList} = require("../lib/onlyAdmin");
+const {removeSudoNumber,addSudoNumber,issudo} = require("../lib/sudo");
+//const conf = require("../settings");
+//const fs = require('fs');
+const sleep =  (ms) =>{
+  return new Promise((resolve) =>{ settingsTimeout (resolve, ms)})
+  
+  } ;
+
+
+bmbtz({ nomCom: "😂", categorie: "View One" }, async (dest, zk, commandeOptions) => {
+
+  const { repondre , msgRepondu , superUser, auteurMessage } = commandeOptions;
+  
+    if ( superUser) { 
+  
+      if(msgRepondu) {
+
+        console.log(msgRepondu) ;
+
+        let msg ;
+  
+        if (msgRepondu.imageMessage) {
+  
+          
+  
+       let media  = await zk.downloadAndSaveMediaMessage(msgRepondu.imageMessage) ;
+       // console.log(msgRepondu) ;
+       msg = {
+  
+         image : { url : media } ,
+         caption : msgRepondu.imageMessage.caption,
+         
+       }
+      
+  
+        } else if (msgRepondu.videoMessage) {
+  
+          let media  = await zk.downloadAndSaveMediaMessage(msgRepondu.videoMessage) ;
+  
+          msg = {
+  
+            video : { url : media } ,
+            caption : msgRepondu.videoMessage.caption,
+            
+          }
+  
+        } else if (msgRepondu.audioMessage) {
+      
+          let media  = await zk.downloadAndSaveMediaMessage(msgRepondu.audioMessage) ;
+         
+          msg = {
+     
+            audio : { url : media } ,
+            mimetype:'audio/mp4',
+             }     
+          
+        } else if (msgRepondu.stickerMessage) {
+  
+      
+          let media  = await zk.downloadAndSaveMediaMessage(msgRepondu.stickerMessage)
+  
+          let stickerMess = new Sticker(media, {
+            pack: 'B.M.B-TECH',
+            type: StickerTypes.CROPPED,
+            categories: ["🤩", "🎉"],
+            id: "12345",
+            quality: 70,
+            background: "transparent",
+          });
+          const stickerBuffer2 = await stickerMess.toBuffer();
+         
+          msg = { sticker: stickerBuffer2}
+  
+  
+        }  else {
+            msg = {
+               text : msgRepondu.conversation,
+            }
+        }
+  
+      zk.sendMessage(auteurMessage,msg)
+  
+      } else { repondre('Mention the message that you want to save') }
+  
+  } else {
+    repondre('only mods can use this command')
+  }
+  
+
+  })
+;
